f49b78daee8984bf4205b4358eaf6b736e9f95ef
3bdulra7manAmir <ethanhunt4d@gmail.com>
2025-11-06 12:20:12 +0200

    update CV download link in Profile component

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -57,7 +57,7 @@ const Profile = () => {
             </a>
             <a
               className={`btn xxs:btn-lg px-6 max-xs:px-2 xxs:py-3 hover:border-picto-primary bg-white duration-300 transition-all hover:text-picto-primary  ms-4 text-xs xxs:text-[14px] sm:text-[16px]`}
-              href="https://drive.google.com/uc?export=download&id=16M3axGj4rvjsb7T2bKcU1pSeGt9ljBwi"
+              href="https://drive.google.com/uc?export=download&id=1VlUTXSmDvHlQPDlOEa3B142BAzw06MBM"
             >
               <FontAwesomeIcon icon={faDownload} /> Download CV
             </a>
